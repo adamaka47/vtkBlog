01d55ae25457541056ea0ddb084c239a9eca57ce
Adam <adamanttv7@yandex.ru>
2020-02-20 22:10:31 +0300

    Сверстал все без адаптива

--- src/js/scriptvar.js
+++ src/js/scriptvar.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var userFeed = new Instafeed({
+  get: 'user',
+  userId: '8780719327',
+  limit: 12,
+  resolution: 'standard_resolution',
+  template: '<a href="{{link}}" class="inst-img" target="_blank" style="background-image:url({{image}})"></a>',
+  accessToken: '8780719327.1677ed0.44bf9b50260f460e994f0f97422b9dfe'
+});
+userFeed.run();
